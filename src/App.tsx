5b5e7ac0ab2d741a9181e8f37761c7885c9f4016
hoangvu6262 <vucao@namlongsoft.net>
2024-10-26 14:06:40 +0700

    get data from json file

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,44 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Paragraph from "./components/Paragraph";
+import { NotificationType, QuestionData } from "./constants/type";
+import Notifications from "./components/Notification";
+import { useNotificationStore } from "./stores/useNotificationStore";
 
 function App() {
+  const [data, setData] = useState<QuestionData | null>(null);
+  const addNotification = useNotificationStore(
+    (state) => state.addNotification
+  );
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await fetch("src/data/question.json", {
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+        });
+
+        const jsonData = await response.json();
+        addNotification("Get data success.", NotificationType.SUCCESS);
+        setData(jsonData);
+      } catch (error) {
+        console.error("Error fetching the data", error);
+      }
+    };
+
+    getData();
+  }, [addNotification]);
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
   return (
     <div>
-      <Paragraph />
+      <Paragraph question={data.question} />
+      <Notifications />
     </div>
   );
 }
